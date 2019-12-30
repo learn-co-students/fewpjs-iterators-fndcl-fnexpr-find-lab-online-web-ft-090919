c82d459e333cdd93d1a84d727510e240cdcd7baa
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-30 13:56:09 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+superbowlWin = function(record){
+  let win = record.find( entry => entry.result === "W")
+  return !!win ? win.year : undefined
+}
