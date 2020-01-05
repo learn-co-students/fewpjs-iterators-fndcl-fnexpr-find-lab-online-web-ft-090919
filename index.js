5cc9599b7b4e9326ee2269b4389f26d91dee101d
Doug Kintop <dkintop@hotmail.com>
2020-01-05 15:42:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-function superBowlWin(array) {
-  array.find(function(bowl) {
-    debugger;
-    return bowl.result === "W";
-  });
-}
-superBowlWin([
-  { year: "2018", result: "L" },
-  { year: "2017", result: "W" },
-  { year: "2016", result: "N/A" }
-]);
+superbowlWin = record => {
+  let result = record.find(record => record.result === "W");
+  return !!result ? result.year : undefined;
+};
