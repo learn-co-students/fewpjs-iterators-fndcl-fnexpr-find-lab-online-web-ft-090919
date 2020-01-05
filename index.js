bad45da3037b656632cd79f88567ba084418fb0a
Doug Kintop <dkintop@hotmail.com>
2020-01-05 15:37:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superBowlWin(array) {
+  array.find(function(bowl) {
+    debugger;
+    return bowl.result === "W";
+  });
 }
+superBowlWin([
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" }
+]);
