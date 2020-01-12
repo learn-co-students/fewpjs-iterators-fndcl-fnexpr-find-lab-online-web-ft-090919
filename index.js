7b4fa598761f600185fbe95749da2f7b52abacd1
dean <deanlloyd64@gmail.com>
2020-01-12 14:50:58 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(records){
+  let record = records.find(function(record){
+    return record['result'] === "W"
+  })
+  return record
+}
