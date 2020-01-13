12f443aa2b4cabcf73ea88724845eab56c61b582
DBuckley0126 <DannyBoy.0126>
2020-01-13 00:26:10 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let foundObject = array.find(object => {
+    if (object.result === "W"){
+      return object
+    }
+  })
+  console.log(foundObject)
+  return foundObject ? foundObject.year : undefined
+} 
